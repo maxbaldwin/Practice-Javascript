e879d66c256b256869ddfbea79e5500bc0bc180c
Max Baldwin <maxrbaldwin@gmail.com>
2016-08-13 11:21:43 -0500

    solved problem 2

--- for-loops.js
+++ for-loops.js
@@ -5,5 +5,15 @@ var count;
 //create for loop with variable above. "Count" starts at 5. Then state where you want it to end, 120.
 //then state by how many numbers you want to count it "5"
 for (count = 5; count <= 120; count = count +5) {
-  document.getElementById("count5").innerHTML += count + " ";
+  document.write(count + " "); 
 };
+
+//Solving problem two, decrementing by 2 and stopping at value 1
+
+//create variable
+var count2;
+//create for loop with variable above number 4096. Then state where you want it to end, 1. 
+//then state by how many numbers you want it to be divided by "2"
+for (count2 = 4096; count2 > 1; count2/=2) {
+  document.write(count2 + " "); 
+} ; 
